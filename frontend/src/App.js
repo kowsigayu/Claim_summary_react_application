a273d072c7b6a8cd6ea4e0bcff787e83c8d39764
kowsigayu <kowsikathiravan@gmail.com>
2020-09-12 14:02:33 +0530

    Add files via upload
    Added styling

---
+++ frontend/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './component/navbar';
+import Login from './component/login';
+import ViewClaim from './component/view_claim';
+import UpdateClaim from './component/update_claim';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Login}/>
+          <Route exact path="/viewclaim" component={ViewClaim}/>
+          <Route exact path="/updateclaim/:id" component={UpdateClaim}/>
+          </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
